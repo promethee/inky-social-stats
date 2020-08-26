9a4bfb288540663ee226d92395ce294fd54fa070
promethee <lunette.francois@gmail.com>
2020-08-26 11:47:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from threading import Thread
 from time import sleep
 import json
-from PIL import Image, ImageDraw, ImageFont
-from inky.auto import auto
 import signal
 from evdev import uinput, UInput, ecodes as e
 import buttonshim
 from UserStats import UserStats
 
-inky_display = auto()
-
-# Uncomment the following if you want to rotate the display 180 degrees
-inky_display.h_flip = True
-inky_display.v_flip = True
-
+def color_scan():
+  buttonshim.set_pixel(0xff, 0x00, 0x00)
+  sleep(0.1)
+  buttonshim.set_pixel(0xff, 0xff, 0x00)
+  sleep(0.1)
+  buttonshim.set_pixel(0x00, 0xff, 0x00)
+  buttonshim.set_pixel(0x00, 0xff, 0xff)
+  sleep(0.1)
+  buttonshim.set_pixel(0x00, 0x00, 0xff)
+  sleep(0.1)
+  buttonshim.set_pixel(0xff, 0x00, 0xff)
+  sleep(0.1)
+  buttonshim.set_pixel(0x00, 0x00, 0x00)
+  sleep(0.1)
 
 KEYCODES = [e.KEY_A, e.KEY_B, e.KEY_C, e.KEY_D, e.KEY_E]
 BUTTONS = [buttonshim.BUTTON_A, buttonshim.BUTTON_B, buttonshim.BUTTON_C, buttonshim.BUTTON_D, buttonshim.BUTTON_E]
 
-img = Image.new("P", inky_display.resolution)
-draw = ImageDraw.Draw(img)
-
-fontsize = 40
-font = ImageFont.truetype("DejaVuSansMono.ttf", fontsize)
-
-draw.text((0, 0), 'Starting', inky_display.BLACK, font)
-inky_display.set_image(img)
-inky_display.show(img)
-
 social_handles = list()
+
+color_scan()
+
+# Everything is fine so far
+buttonshim.set_pixel(0x00, 0xff, 0x00)
 
 print('[?] will try to open file named "twitter.json"')
 try:
@@ -37,9 +39,12 @@ try:
     data = json.load(json_file)
     print('[✓] file found with', len(data), 'handle(s)')
     for i in range(len(data)):
+      buttonshim.set_pixel(0x00, 0x00, 0x00)
       print('[✓] twitter handle found at position', i, data[i])
       social_handles.append(data[i])
+      buttonshim.set_pixel(0x00, 0x00, 0xff)
 except FileNotFoundError as e:
+  buttonshim.set_pixel(0xff, 0x00, 0x00)
   print('[⨉] missing file named "twitter.json"')
 
 print('[✓] handles loaded')
@@ -54,14 +59,24 @@ if len(Users) > 0:
   Users[0].toggle(True)
   Users[0].display()
 
+def update_users():
+  while True:
+    color_scan()
+    for user in Users:
+      user.update()
+    sleep(60)
+
+user_thread = Thread(target=update_users)
+user_thread.start()
+
 @buttonshim.on_press(BUTTONS)
 def button_p_handler(button, pressed):
-    for i in range(len(Users)):
-      Users[i].toggle(i == BUTTONS.index(button))
-    Users[BUTTONS.index(button)].update()
+    buttonshim.set_pixel(0xff, 0xff, 0x00)
   
 @buttonshim.on_release(BUTTONS)
 def button_r_handler(button, pressed):
+    color_scan()
+    buttonshim.set_pixel(0x00, 0x00, 0x00)
     Users[BUTTONS.index(button)].display()
 
 signal.pause()
